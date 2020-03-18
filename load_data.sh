1847bbc2b46756278f6602778ae54f4d3288af5e
Jeff Wintersinger <jeff.git@wintersinger.com>
2020-03-18 05:44:24 -0400

    Update data.

--- load_data.sh
+++ load_data.sh
@@ -1,2 +1,2 @@
 cases_url='https://docs.google.com/spreadsheets/d/1D6okqtBS3S2NRC7GFVHzaZ67DuTw7LX49-fqSLwJyeo/export?format=csv&id=1D6okqtBS3S2NRC7GFVHzaZ67DuTw7LX49-fqSLwJyeo&gid=942958991' 
-curl --silent "$cases_url" | tail -n+2 | python3 munge_case_data.py cases.csv
+curl --silent "$cases_url" | tail -n+3 | python3 munge_case_data.py cases.csv
